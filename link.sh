965e571a12f9eab596626046a851095579c9c17e
No Name <noname@mailinator.com>
2020-06-22 12:39:39 +0200

    updated slicerrc.py. does not work, however

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SOURCE_DIR=$(dirname "$0")
+SOURCE_DIR=`dirname "$(readlink -f "$0")"`
 cd ~ 
 ln -is "$SOURCE_DIR/slicerrc.py" ".slicerrc.py"
 
--- slicerrc.py
+++ slicerrc.py
@@ -9,45 +9,66 @@
 # slicer.util.mainWindow().moduleSelector().selectModule('SegmentEditor')
 #
 
-#set the default model save format to stl (from vtk)
-defaultModelStorageNode = slicer.vtkMRMLModelStorageNode()
-defaultModelStorageNode.SetUseCompression(0)
-defaultModelStorageNode.SetDefaultWriteFileExtension('stl')
-slicer.mrmlScene.AddDefaultNode(defaultModelStorageNode)
+################################################################################
 
-#disable interpolation of the volumes by default
-#def NoInterpolate(caller,event):
-#  for node in slicer.util.getNodes('*').values():
-#    if node.IsA('vtkMRMLScalarVolumeDisplayNode'):
-#      node.SetInterpolate(0)
-#slicer.mrmlScene.AddObserver(slicer.mrmlScene.NodeAddedEvent, NoInterpolate)
+def infoSlicerRC(s):
+    import logging
+    logging.info(".slicerrc.py: " + s) 
 
-#hide SLicer logo in module tab
-slicer.util.findChild(slicer.util.mainWindow(), 'LogoLabel').visible = False
 
-#collapse Data Probe tab by default to save space modules tab
-slicer.util.findChild(slicer.util.mainWindow(), name='DataProbeCollapsibleWidget').collapsed = True
 
 ################################################################################
+
+# set the default model save format to .stl (defautl: .vtk)
+def setModelStorageFormat(ext):
+    defaultModelStorageNode = slicer.vtkMRMLModelStorageNode()
+    defaultModelStorageNode.SetUseCompression(0)
+    defaultModelStorageNode.SetDefaultWriteFileExtension( ext )
+    slicer.mrmlScene.AddDefaultNode(defaultModelStorageNode)
+
+
+
+################################################################################
+
+# enable/disable volume interpolation 
+def setVolumeInterpolation(v):
+    def setInterpolationAll(v):
+      for node in slicer.util.getNodes('*').values():
+        if node.IsA('vtkMRMLScalarVolumeDisplayNode'):
+          node.SetInterpolate( v )
+    def interpolator(caller,event):
+      setInterpolationAll(v) # ^ TODO: use Node in 'event'
+    # set value for all current nodes:
+    setInterpolationAll( v )
+    # observe new volumes 
+    slicer.mrmlScene.AddObserver(slicer.mrmlScene.NodeAddedEvent, interpolator)
+
+
+
+################################################################################
+
 # toggle 3D fullscreen
-
-prevLayout = slicer.vtkMRMLLayoutNode.SlicerLayoutDefaultView
-
-def toggle3DFullscreen():
-    global prevLayout
+def createToggle3D():
     lm = slicer.app.layoutManager()
-    if lm.layout == slicer.vtkMRMLLayoutNode.SlicerLayoutOneUp3DView:
-        lm.setLayout( prevLayout )
-    else:
-        prevLayout = lm.layout
-        lm.setLayout( slicer.vtkMRMLLayoutNode.SlicerLayoutOneUp3DView )
+    layoutPrev = lm.layout 
+    layout3D = slicer.vtkMRMLLayoutNode.SlicerLayoutOneUp3DView
+    def toggle3D():
+        layoutPrev # FIXME: global? no, according to https://stackoverflow.com/a/279586
+        if lm.layout == layout3D:
+            lm.setLayout( layoutPrev )
+        else:
+            layoutPrev = lm.layout
+            lm.setLayout( layout3D )
+    # create GUI button
+    widget = mainWindow().findChild('QToolBar', 'ViewToolBar')
+    action = widget.addAction("3D") 
+    action.setToolTip('Toggle 3D Fullscreen')
+    action.connect('triggered()', lambda: toggle3DFullscreen() )
+    # create shortcut: f 
+    shortcutToggle3D = qt.QShortcut( mainWindow() )
+    shortcutToggle3D.setKey( qt.QKeySequence('f') )
+    shortcutToggle3D.connect( 'activated()', toggle3D )
 
-
-# create GUI button
-widget = mainWindow().findChild('QToolBar', 'ViewToolBar')
-action = widget.addAction("Toggle 3D") 
-action.setToolTip('Toggle 3D Fullscreen')
-action.connect('triggered()', lambda: toggle3DFullscreen() )
 
 
 ################################################################################
@@ -108,89 +129,64 @@ LAYOUTXML_MANUALSEGMENTATION = """
 </layout>
 """
 
-# out of the blue
-LAYOUTID_MANUALSEGMENTATION_3D  = 440
-LAYOUTID_MANUALSEGMENTATION = 442
+# out-of-the-blue values
+LAYOUTID_MANUALSEGMENTATION_3D = 440
+LAYOUTID_MANUALSEGMENTATION    = 442
 
 
+# switch layout
 def setLayout(idx):
     layoutManager = slicer.app.layoutManager()
     layoutManager.setLayout( idx )
 
 
-#viewToolBar = mainWindow().findChild('QToolBar', 'ViewToolBar')
-#layoutMenu = viewToolBar.widgetForAction(viewToolBar.actions()[0]).menu()
-#layoutSwitchActionParent = layoutMenu  # use `layoutMenu` to add inside layout list, use `viewToolBar` to add next the standard layout list
-#layoutSwitchAction = layoutSwitchActionParent.addAction("My view") # add inside layout list
-#layoutSwitchAction.setData(layoutId)
-#layoutSwitchAction.setIcon(qt.QIcon(':Icons/Go.png'))
-#layoutSwitchAction.setToolTip('3D and slice view')
-#layoutSwitchAction.connect('triggered()', lambda layoutId = customLayoutId: slicer.app.layoutManager().setLayout(layoutId))
+
+# global variables to access our layout actions
+actionSegmentation3D = None
+actionSegmentation   = None
 
 
-################################################################################
-# toggle segmentation fullscreen
+# callback for action
+def triggerLayoutManualSegmentation():
+    # set layout
+    setLayout( LAYOUTID_MANUALSEGMENTATION )
+    # TODO: turn off 3D render
 
-prevLayoutSeg = LAYOUTID_MANUALSEGMENTATION_3D
+# callback for action
+def triggerLayoutManualSegmentation3D():
+    # set layout
+    setLayout( LAYOUTID_MANUALSEGMENTATION_3D )
+    # TODO: turn on 3D render
 
-# jump to fullscreen segmentation (axial)
-def toggleSegmentationFullscreen():
-    global prevLayoutSeg
+# callback for shortcut
+def toggleLayoutManualSegmentation():
     lm = slicer.app.layoutManager()
-    if lm.layout == LAYOUTID_MANUALSEGMENTATION:
-        lm.setLayout( prevLayoutSeg )
-    else:
-        prevLayoutSeg = lm.layout
-        lm.setLayout( LAYOUTID_MANUALSEGMENTATION )
-
-# also add a GUI button for this
-widget = mainWindow().findChild('QToolBar', 'ViewToolBar')
-action = widget.addAction("Toggle manual segmentation") 
-action.setToolTip('Toggle segmentation view')
-action.connect('triggered()', lambda: toggleSegmentationFullscreen() )
-
-################################################################################
+    if lm.layout == LAYOUTID_MANUALSEGMENTATION_3D:
+        actionSegmentation.triggered()
+    else: 
+        actionSegmentation3D.triggered()
 
 
-# create layout: seg+3D 
-def createLayoutSeg():
-    # add layout
+# create our custom layouts, add actions in layout menu, create shortcut
+def createCustomLayouts():
+    # create layouts
     slicer.app.layoutManager().layoutLogic().GetLayoutNode().AddLayoutDescription( LAYOUTID_MANUALSEGMENTATION_3D, LAYOUTXML_MANUALSEGMENTATION_3D )
-    # create menu entry for layout
-    viewMenu = mainWindow().findChild('QToolBar', 'ViewToolBar')
-    widget = viewMenu.widgetForAction( viewMenu.actions()[0] ).menu()
-    action = widget.addAction("Segmentation") 
-    action.setToolTip('Layout for manual segmentation')
-    action.connect('triggered()', lambda layoutId = LAYOUTID_MANUALSEGMENTATION_3D: slicer.app.layoutManager().setLayout( layoutId) )
-
-def createLayoutSegAxial():
-    # add layout
     slicer.app.layoutManager().layoutLogic().GetLayoutNode().AddLayoutDescription( LAYOUTID_MANUALSEGMENTATION, LAYOUTXML_MANUALSEGMENTATION )
-    # create menu entry for layout
-    # FIXME: for some reason, this crashes the whole application. i guess it has something to do with the 'createLayoutSeg()' above
-    #viewMenu = mainWindow().findChild('QToolBar', 'ViewToolBar')
-    #widget = viewMenu.widgetForAction( viewMenu.actions()[0] ).menu()
-    #action = widget.addAction("Axial Segmentation") 
-    #action.setToolTip('Axial Segmentation (fullscreen)')
-    #action.connect('triggered()', lambda layoutId = LAYOUTID_MANUALSEGMENTATION: slicer.app.layoutManager().setLayout( layoutId) )
-    #action.connect('triggered()', lambda layoutId = LAYOUTID_MANUALSEGMENTATION: print('hello') )
-
-
-
-################################################################################
-# shortcuts keyboard keys
-
-# custom shortcuts
-def createShortcuts():
-    # create shortcut: g for segmentation fullscreen
-    shortcutToggleSeg = qt.QShortcut( slicer.util.mainWindow() )
+    # create menu entries
+    global actionSegmentation3D
+    global actionSegmentation
+    actionSegmentation3D = mainWindow().findChild('QMenu', 'LayoutMenu').addAction( "Segmentation+3D" ) # TODO: create Icon: #.setIcon(qt.QIcon(':Icons/Go.png'))
+    actionSegmentation3D.setToolTip("Manual Segmentation")
+    actionSegmentation3D.connect('triggered()', lambda: triggerLayoutManualSegmentation3D() )
+    actionSegmentation = mainWindow().findChild('QMenu', 'LayoutMenu').addAction( "Segmentation" ) # TODO: create Icon: #.setIcon(qt.QIcon(':Icons/Go.png'))
+    actionSegmentation.setToolTip("Manual Segmentation, fullscreen")
+    actionSegmentation.connect('triggered()', lambda: triggerLayoutManualSegmentation() )
+    # create toggle shortcut
+    shortcutToggleSeg = qt.QShortcut( mainWindow() )
     shortcutToggleSeg.setKey( qt.QKeySequence('g') )
-    shortcutToggleSeg.connect( 'activated()', toggleSegmentationFullscreen )
-
-    # create shortcut: f for 3D fullscreen
-    shortcutToggle3D = qt.QShortcut( slicer.util.mainWindow() )
-    shortcutToggle3D.setKey( qt.QKeySequence('f') )
-    shortcutToggle3D.connect( 'activated()', toggle3DFullscreen )
+    shortcutToggleSeg.connect( 'activated()', lambda: toggleLayoutManualSegmentation() )
+    # set custom layout right now. this makes sure volumes are loaded into our custom Slice Views
+    #actionSegmentation3D.triggered()
 
 
 
@@ -198,6 +194,29 @@ def createShortcuts():
 ################################################################################
 # init
 
-createLayoutSeg()
-createLayoutSegAxial()
-createShortcuts()
+def init():
+    # no interpolation
+    setVolumeInterpolation(False)
+    infoSlicerRC("Volume interpolation off. enable with 'setVolumeInterpolation( True )")
+    # default model format
+    setModelStorageFormat( 'stl' )
+    infoSlicerRC("Default model storage format: .stl")
+    # no dataprobe 
+    setDataProbeVisible( False )
+    infoSlicerRC("Data probe hidden. enable with 'setDataProbeVisible( True )'")
+    # no logo
+    setApplicationLogoVisible( False )
+    # create and set custom layout
+    createCustomLayouts()
+    # create toggle 3D fullscreen 
+    createToggle3D()
+    infoSlicerRC("Custom layout created.")
+    # set default module: Data
+    selectModule("Data")
+    infoSlicerRC("Switched to module 'Data'")
+
+
+
+# lets run
+init()
+
